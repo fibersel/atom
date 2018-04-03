88765c25f83227444137586079f09c4e68bbd547
fibersell <p90abi@mail.ru>
2018-04-03 19:56:27 +0300

    need to fix connection page

--- game/MatchMaker/src/main/java/matchmaker/MatchMaker.java
+++ game/MatchMaker/src/main/java/matchmaker/MatchMaker.java
@@ -1,12 +1,19 @@
 package matchmaker;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -23,7 +30,6 @@ public class MatchMaker {
     @Autowired
     private MatchMakerRepository repository;
 
-
     private static boolean enabled = false;
 
     /*
@@ -35,12 +41,22 @@ public class MatchMaker {
             method = RequestMethod.POST,
             consumes = org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity join(@RequestParam("name") String name) throws InterruptedException{
+        Document document;
         playersQueue.offer(name);
         int rank = repository.getUserRank(name);
         while (!playersId.containsKey(name))
             Thread.sleep(10);
         Long id = playersId.get(name);
-        return ResponseEntity.ok(id);
+        try {
+            File file = new File("src/main/resources/static/connection.html");
+            document = Jsoup.parse(file , "ISO-8859-1");
+            Element element = document.getElementById("id");
+            element.val(id.toString());
+            return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(document.toString());
+        } catch (IOException e) {
+            System.out.println("Failed to parse HTML!");
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 
