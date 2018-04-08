5b80b3474835118ef77af91f2245c73a9f359d39
ivanmakarychev <34499928+ivanmakarychev@users.noreply.github.com>
2018-04-09 00:46:18 +0300

    Matchmaker monitoring (#5)
    * Добавил не учтенные тест-кейсы:
    1) Прямоугольники пересекаются нижним левым и верним правым углами,
    2) Прямоугольники расположены крест-накрест.
    
    * logo
    
    * lecture01 new gradle structure
    
    * presentation fixes
    
    * presentation + test improvements for lectures 01 & 02
    
    * lecture02 new gradle structure
    
    * changed .gitignores and travis
    
    * gradlew for lecture 02
    
    * presentation gradle part changed
    
    * props for new java version
    
    * merge
    
    * presentation
    
    * presentation fix
    
    * README
    
    * README
    
    * README
    
    * README
    
    * gradlew update
    
    * Update PITCHME.md
    
    * compilation fix
    
    * presentation null fix
    
    * lecture03 fixes
    
    * new project structure migration
    
    * merge
    
    * remove old md
    
    * logger dependencies fix
    
    * presentation and practice fixes
    
    * hw2
    
    * hw2
    
    * rm full dict
    
    * practice fixes
    
    * presentation fixes
    
    * presentation fixes
    
    * presentation fixes
    
    * checkstyle and presentation fix
    
    * updated presentation and practice for new style and gradle structure
    
    * presentation fix
    
    * presentation fix
    
    * presentation fix
    
    * fix presentation images
    
    * fix presentation images
    
    * fix presentation
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04
    
    * lecture04 presentation fixes
    
    * lecture04 presentation fixes #2
    
    * Bugfix. Lecture04 login test
    
    * Bugfix. Change values to default
    
    * Lecture04 changes
    
    * Feature/game task (#869)
    
    * task:
    
    * Update readme11.md
    
    * Update readme11.md
    
    * Update readme11.md
    
    * Create README.md
    
    * Update README.md
    
    * Add files via upload
    
    * Update README.md
    
    * Delete readme11.md
    
    * Update README.md
    
    * Add files via upload
    
    * task fixes
    
    * update task
    
    * corrections
    
    * game
    
    * label and matchmaker logics made
    
    * another try
    
    * new version
    
    * matchmaker fully completed without bonus
    
    * База данных (#1)
    
    Отлично
    
    * DB added and tests fixed
    
    * Connection page (#4)
    
    * need to fix connection page
    
    * mediaType changed
    
    * connection page made
    
    * Change html from JS (#3)
    
    Loading indicator
    
    * code cleaned,tests fixed;
    
    * ranikng added
    
    * tests fixed,ranking fixed
    
    * Monitoring
    Added schema to DB
    Improved tables in DB
    Use embedded DB for tests
    
    * Repository tests

---
+++ game/MatchMaker/src/main/java/matchmaker/MatchMakerDaemon.java
@@ -0,0 +1,90 @@
+package matchmaker;
+
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.swing.plaf.TableHeaderUI;
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Scope("prototype")
+public class MatchMakerDaemon implements Runnable {
+
+
+    private BlockingQueue<String> playersQueue;
+    @Autowired
+    private OkHttpClient client;
+    @Autowired
+    private ConcurrentHashMap<String,Long> playersId;
+    @Autowired
+    private MatchMakerRepository repository;
+
+    private static final String PROTOCOL = "http://";
+    private static final String HOST = "localhost";
+    private static final String PORT = ":8080";
+    private static int MAX_NUMBER_OF_PLAYERS = 4;
+
+    private int numberOfPlayers;
+
+    public void setPlayersQueue(BlockingQueue<String> playersQueue) {
+        this.playersQueue = playersQueue;
+    }
+
+    @Override
+    public void run() {
+        numberOfPlayers = 0;
+        int index = 0;
+        Long id;
+
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        Request request;
+        Response response;
+
+        String[] players = new String[MAX_NUMBER_OF_PLAYERS];
+
+        while (!Thread.interrupted()){
+
+            if (!playersQueue.isEmpty()){
+                System.out.println("YET ANPTHER USER,QID:" + Thread.currentThread().getName());
+                try {
+                    players[index++] = playersQueue.poll(10_000, TimeUnit.SECONDS);
+                } catch (InterruptedException e){
+                    return;
+                }
+                numberOfPlayers++;
+            }
+
+            if(numberOfPlayers == MAX_NUMBER_OF_PLAYERS){
+                request = new Request.Builder()
+                        .post(RequestBody.create(mediaType , "playerCount=" + numberOfPlayers))
+                        .url(PROTOCOL + HOST + PORT + "/game/create")
+                        .build();
+                try {
+                    response = client.newCall(request).execute();
+                } catch (IOException e){
+                    return;
+                }
+                try {
+                    id = Long.parseLong(response.body().string());
+                } catch (Exception e){
+                    return;
+                }
+                index = 0;
+                numberOfPlayers = 0;
+                for(String names: players)
+                    playersId.put(names, id);
+                repository.saveGameSession(id, players);
+            }
+        }
+    }
+
+    int getNumberOfWaitingPlayers() {
+        return numberOfPlayers + playersQueue.size();
+    }
+}
---
+++ game/MatchMaker/src/test/java/matchmaker/MatchMakerTest.java
@@ -0,0 +1,70 @@
+package matchmaker;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatchMakerTest {
+
+    @Autowired
+    private ApplicationContext ctx;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private ConcurrentHashMap<Long,Integer> returnedRequests;
+
+    private static final int NUMBER_OF_REQESTS = 16;
+
+    @Test
+    @Ignore
+    public void matchMakerTest() throws InterruptedException{
+        Collection<Thread> list = new LinkedList<>();
+        int rank = 5;
+        for (int i = 0;i < NUMBER_OF_REQESTS;i++) {
+            if (i % 4 == 0 && i > 0)
+                rank += 10;
+            TestClient client = ctx.getBean(TestClient.class);
+            client.setRank(rank);
+            Thread thread = new Thread(client);
+            list.add(thread);
+            thread.start();
+        }
+        for (Thread thread:list)
+            thread.join();
+        int ctr = 0;
+        for (Long key: returnedRequests.keySet())
+            ctr += returnedRequests.get(key);
+        Assert.assertTrue(ctr == NUMBER_OF_REQESTS);
+    }
+
+    @Test
+    @Ignore
+    public void matchMakerIncompleteSessionTest() throws InterruptedException{
+        Collection<Thread> list = new LinkedList<>();
+        for (int i = 0;i < 7;i++) {
+            list.add(new Thread(new TestClient()));
+        }
+        for (Thread thread:list)
+            thread.start();
+        for (Thread thread:list)
+            thread.join();
+    }
+
+
+
+
+
+}
