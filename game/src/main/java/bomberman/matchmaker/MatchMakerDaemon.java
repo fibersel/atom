e4b23eaffd49d967bc80dcfc617ef573217b0b46
S_Vasilyev <30511109+A1baJ@users.noreply.github.com>
2018-05-01 14:25:26 +0300

    daemon refactor (#23)

--- game/src/main/java/bomberman/matchmaker/MatchMakerDaemon.java
+++ game/src/main/java/bomberman/matchmaker/MatchMakerDaemon.java
@@ -20,7 +20,6 @@ public class MatchMakerDaemon implements Runnable {
 
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(MatchMakerDaemon.class);
 
-    private BlockingQueue<String> playersQueue;
     @Autowired
     private OkHttpClient client;
     @Autowired
@@ -28,13 +27,14 @@ public class MatchMakerDaemon implements Runnable {
     @Autowired
     private MatchMakerRepository repository;
 
+    private BlockingQueue<String> playersQueue;
+    private int numberOfPlayers;
+
     private static final String PROTOCOL = "http://";
     private static final String HOST = "localhost";
     private static final String PORT = ":8080";
-    private static int MAX_NUMBER_OF_PLAYERS = 4;
+    private static final int MAX_NUMBER_OF_PLAYERS = 4;
     private static final long WAIT_TIME = 10_000_000_000L;
-
-    private int numberOfPlayers;
 
     public void setPlayersQueue(BlockingQueue<String> playersQueue) {
         this.playersQueue = playersQueue;
@@ -42,26 +42,29 @@ public class MatchMakerDaemon implements Runnable {
 
     @Override
     public void run() {
+        log.info(Thread.currentThread().getName() + " Started!");
+
         numberOfPlayers = 0;
+
+        while (!Thread.interrupted()) {
+            makeSession(addPlayers());
+        }
+    }
+
+    String[] addPlayers () {
+        String[] players = new String[MAX_NUMBER_OF_PLAYERS];
         int index = 0;
         long lastTime = Long.MAX_VALUE;
         long curTime = System.nanoTime();
-        Long id;
 
-        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
-        Request request;
-        Response response;
-
-        String[] players = new String[MAX_NUMBER_OF_PLAYERS];
-        log.info(Thread.currentThread().getName() + " Started!");
-        while (!Thread.interrupted()) {
-
+        while (!((numberOfPlayers == MAX_NUMBER_OF_PLAYERS) || (numberOfPlayers > 1)
+                && (((curTime = System.nanoTime()) - lastTime) > WAIT_TIME))) {
             if (!playersQueue.isEmpty()) {
                 try {
                     players[index++] = playersQueue.poll(10_000, TimeUnit.SECONDS);
                 } catch (InterruptedException e) {
                     log.error(e.getMessage());
-                    return;
+                    return players;
                 }
                 numberOfPlayers++;
 
@@ -69,37 +72,42 @@ public class MatchMakerDaemon implements Runnable {
                     lastTime = System.nanoTime();
                 }
             }
-
-            if ((numberOfPlayers == MAX_NUMBER_OF_PLAYERS) || (numberOfPlayers > 1)
-                    && (((curTime = System.nanoTime()) - lastTime) > WAIT_TIME)) {
-                String[] playersInThisSession = new String[numberOfPlayers];
-                for (int i = 0; i < numberOfPlayers; i++)
-                    playersInThisSession[i] = players[i];
-
-                request = new Request.Builder()
-                        .post(RequestBody.create(mediaType , "playerCount=" + numberOfPlayers))
-                        .url(PROTOCOL + HOST + PORT + "/game/create")
-                        .build();
-                try {
-                    response = client.newCall(request).execute();
-                } catch (IOException e) {
-                    log.error(e.getMessage());
-                    return;
-                }
-                try {
-                    id = Long.parseLong(response.body().string());
-                } catch (Exception e) {
-                    log.error(e.getMessage());
-                    return;
-                }
-                index = 0;
-                numberOfPlayers = 0;
-                for (String names: playersInThisSession)
-                    playersId.put(names, id);
-                repository.saveGameSession(id, playersInThisSession);
-
-            }
         }
+
+        String[] finalList = new String[numberOfPlayers];
+        for (int i = 0; i < numberOfPlayers; i++)
+            finalList[i] = players[i];
+
+        return finalList;
+    }
+
+    void makeSession (String[] players) {
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        Request request;
+        Response response;
+        Long id;
+
+        request = new Request.Builder()
+                .post(RequestBody.create(mediaType , "playerCount=" + numberOfPlayers))
+                .url(PROTOCOL + HOST + PORT + "/game/create")
+                .build();
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return;
+        }
+        try {
+            id = Long.parseLong(response.body().string());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return;
+        }
+
+        for (String names: players)
+            playersId.put(names, id);
+        repository.saveGameSession(id, players);
+        numberOfPlayers = 0;
     }
 
     int getNumberOfWaitingPlayers() {
