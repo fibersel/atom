332d72d648510ce07d6806716edef415fb669a59
ivanmakarychev <34499928+ivanmakarychev@users.noreply.github.com>
2018-05-12 20:56:39 +0300

    Bonuses (#27)
    Game over

--- game/src/main/java/bomberman/gameservice/ConnectionPool.java
+++ game/src/main/java/bomberman/gameservice/ConnectionPool.java
@@ -1,27 +1,45 @@
 package bomberman.gameservice;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
+import bomberman.model.Character;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConnectionPool {
 
-    Set<WebSocketSession> sessions = new HashSet<>();
+    //Set<WebSocketSession> sessions = new HashSet<>();
+    Map<Character,WebSocketSession> sessionMap = new HashMap<>();
 
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(ConnectionPool.class);
 
-    public void setSession(WebSocketSession session) {
-        this.sessions.add(session);
+    public void setSession(Character player, WebSocketSession session) {
+        this.sessionMap.put(player, session);
     }
 
     public void broadcast(String msg) throws IOException {
-        for (WebSocketSession session : sessions) {
+        for (WebSocketSession session : sessionMap.values()) {
             session.sendMessage(new TextMessage(msg));
         }
-        log.info("Sent message: " + msg);
+        log.debug("Sent message: " + msg);
+    }
+
+    public void sendMessage(Character player, String msg) throws IOException {
+        sessionMap.get(player).sendMessage(new TextMessage(msg));
+        log.info("Sent message to {}: {}", player.getOwner(), msg);
+    }
+
+    public void closeAllConnections() {
+        for (WebSocketSession session : sessionMap.values()) {
+            try {
+                session.close(CloseStatus.GOING_AWAY);
+            } catch (IOException ignored) {
+            }
+        }
     }
 }
